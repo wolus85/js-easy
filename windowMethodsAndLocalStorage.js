e5d277231b9a356fadc116320c11313aacc06045
Paweł Wolski <pawelwolski3@gmail.com>
2019-10-03 13:26:37 +0200

    add local storage to WindowMethods

---
+++ windowMethodsAndLocalStorage.js
@@ -0,0 +1,35 @@
+let arrayOfPeople = JSON.parse(localStorage.getItem('person'))||[]
+
+const askForName = function () {
+  const name = window.prompt('Write Your Name')
+  return name
+}
+const name = askForName()
+
+const askForLastName = function () {
+  const lastname = window.prompt('Write Your Lastname')
+  return lastname
+}
+
+const lastname = askForLastName()
+
+let hello = window.alert(`Hello ${name} ${lastname}`)
+
+
+const addP = function () {
+
+  const persondata = name + ' ' + lastname
+  console.log(persondata)
+ 
+  arrayOfPeople.push(persondata)
+  console.log(arrayOfPeople)
+
+  localStorage.setItem('person',JSON.stringify(arrayOfPeople))
+}
+
+
+addP()
+
+
+
+
