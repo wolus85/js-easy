bb517f60770994387b0672c8e40cb0d2d8e22c09
Paweł Wolski <pawelwolski3@gmail.com>
2019-10-05 13:12:12 +0200

    finish

--- fly.js
+++ fly.js
@@ -3,28 +3,33 @@ const time = 40
 let position = 0
 
 const move = function () {
-  const flyTime = time/300
+  const flyTime = time / 300
   const displacement = flyTime * velocity
   position += displacement
 }
 
 const render = function () {
-document.body.innerText = ''
+  document.body.innerText = ''
 
-const fly = document.createElement('div')
-fly.style.backgroundColor = 'black'
-fly.style.width = '50px' 
-fly.style.height = '50px'
-fly.style.position = 'absolute'
-fly.style.left = position + 'px'
+  const fly = document.createElement('div')
+  fly.style.backgroundColor = 'black'
+  fly.style.width = '50px'
+  fly.style.height = '50px'
+  fly.style.position = 'absolute'
+  fly.style.left = position + 'px'
 
-document.body.appendChild(fly)
+  document.body.appendChild(fly)
 }
 
 
-setInterval (function(){move(),
-render()},time) 
+setInterval(function () {
+move(),
+  render()
+}, time)
 
-  
+
 render()
+
+
+
 
