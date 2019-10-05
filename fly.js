279d7c3a5796a8d79f81b9671fd4ddd46a45e8aa
Paweł Wolski <pawelwolski3@gmail.com>
2019-10-05 11:34:54 +0200

    add setinterval and functions

--- fly.js
+++ fly.js
@@ -1,8 +1,30 @@
-velocity = 20
-time = 40
-position = 0
+const velocity = 20
+const time = 40
+let position = 0
+
+const move = function () {
+  const flyTime = time/300
+  const displacement = flyTime * velocity
+  position += displacement
+}
+
+const render = function () {
+document.body.innerText = ''
+
+const fly = document.createElement('div')
+fly.style.backgroundColor = 'black'
+fly.style.width = '50px' 
+fly.style.height = '50px'
+fly.style.position = 'absolute'
+fly.style.left = position + 'px'
+
+document.body.appendChild(fly)
+}
 
 
+setInterval (function(){move(),
+render()},time) 
 
+  
+render()
 
-const move = function
